d1c4a3d0ed4a1e1d5bbf90841abe159f5fe2bfac
VelogaWhite <gunyok2515@gmail.com>
2026-03-12 15:34:26 +0700

    Add models.py

--- election_app/models.py
+++ election_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Election(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=255)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    voter_id = models.CharField(max_length=255)
