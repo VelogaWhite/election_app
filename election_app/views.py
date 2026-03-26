b96b40fa3375a4f6467ee121bbaa7aba0246be33
VelogaWhite <gunyok2515@gmail.com>
2026-03-27 05:47:52 +0700

    HomePageTest is now green

--- election_app/views.py
+++ election_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# ฟังก์ชันหน้าแรกที่เราเพิ่งสร้างเพื่อตอบโจทย์เทสต์
+def home(request):
+    return render(request, 'home.html')
+
+# ฟังก์ชันเปล่าๆ (Dummy) เพื่อไม่ให้ urls.py พัง
+def vote(request):
+    pass
+
+def results(request):
+    pass
